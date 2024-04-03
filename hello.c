baca252de822f687b9d5d7389f17770201046653
Albin Alvelius <albin.alvelius@gmail.com>
2024-04-03 23:23:27 +0200

    first commit

---
+++ hello.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define     x_res   128
+#define     y_res   128
+
+struct vec2F {
+    float x;
+    float y;
+};
+
+int **displayMatrix;
+
+void initDisplayMatrix() {
+    for (int i = 0; i < x_res; i++) {
+
+    }
+    
+}
+
+void printDisplayMatrix() {
+    for (int i = 0; i < x_res; i++) {
+        
+    }
+    
+}
+
+int main(int argc, char const *argv[]) {
+
+    printf("sus %f \n", 1.0);
+    return 0;
+}
